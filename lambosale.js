33c6e3886ed2b8639890ed79a936bd9220205ae7
ilyuhalobodenko <insayd2k@gmail.com>
2024-11-25 18:01:54 +0200

    Add files via upload

---
+++ lambosale.js
@@ -0,0 +1,6 @@
+const carname = "Lamborghini Revuelto";
+const caryear = 2024;
+let carprice = 350000;
+let availabel = true;
+let buyername = null;
+console.log (`Назва: ${carname}, Рік: ${caryear}, Ціна: ${carprice} Євро, Доступ: ${availabel}, Покупець: ${buyername}`)
