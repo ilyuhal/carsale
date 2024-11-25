6abc291e077f724f108208f7106c5b53c34c3a94
ilyuhalobodenko <insayd2k@gmail.com>
2024-11-25 18:05:03 +0200

    Update lambosale.js

--- lambosale.js
+++ lambosale.js
@@ -1,6 +1,7 @@
 const carname = "Lamborghini Revuelto";
+const carcolor = "purple";
 const caryear = 2024;
 let carprice = 350000;
 let availabel = true;
 let buyername = null;
-console.log (`Назва: ${carname}, Рік: ${caryear}, Ціна: ${carprice} Євро, Доступ: ${availabel}, Покупець: ${buyername}`)
+console.log (`Назва: ${carname}, Колір: ${carcolor}, Рік: ${caryear}, Ціна: ${carprice} Євро, Доступ: ${availabel}, Покупець: ${buyername}`)
